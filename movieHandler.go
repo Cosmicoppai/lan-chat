e3dbf7c15e4da00a230b9e25e884a6b135a07327
CosmicOppai <oppaiharem69@gmail.com>
2021-11-23 01:19:16 +0530

    added the handler for static files

--- movieHandler.go
+++ movieHandler.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 )
 
-func StaticHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/html")
-	http.ServeFile(w, r, "./templates/main.html")
+func StaticPages(w http.ResponseWriter, r *http.Request) { // To server static pages
+	fileLocation := strings.TrimPrefix(r.URL.Path, "/static/")
+	http.ServeFile(w, r, "./templates/"+fileLocation)
 }
 
-func MovieHandler(w http.ResponseWriter, r *http.Request) {
-	movie := strings.TrimPrefix(r.URL.Path, "/movie/")
+func Home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	fn := r.URL.Path
+	if fn == "/" {
+		http.ServeFile(w, r, "./templates/index.html")
+	} else if fn == "/favicon.ico" {
+		http.Redirect(w, r, "/static"+fn, 301)
+	} else {
+		file := fn + ".html"
+		http.ServeFile(w, r, "./templates/"+file)
+	}
+}
+
+func GetMovie(w http.ResponseWriter, r *http.Request) {
+	movie := strings.TrimPrefix(r.URL.Path, "/get_movie/")
+	fmt.Println(movie)
 	w.Header().Set("content-type", "video/mp4")
 	w.Header().Set("accept-ranges", "bytes")
 	http.ServeFile(w, r, "./movie/"+movie)
