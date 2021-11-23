9125ed9b98938802e9097c6dd39281e8a9b35cc4
Sushant <sushantshetty2001@gmail.com>
2021-11-23 15:08:28 +0530

    subtitle

--- templates/js/script.js
+++ templates/js/script.js
@@ -8,5 +8,16 @@ fetch('/movie_name')
         let result = x.substring(0, remove_after);
         document.getElementById('movieName').innerHTML += result
         let movieSrc = document.getElementById('source')
-        movieSrc.setAttribute('src', '/movie/' + result);
+        movieSrc.setAttribute('src', '/get_movie/' + x);
+        fetch('/get_sub/'+ result)
+            .then(data => {
+                return data.json();
+            })
+            .then(post => {
+                let x = post['subtitle']
+                let subtitlesSrc = document.getElementById('subtitle')
+                subtitlesSrc.setAttribute('src', '/get_sub/' + x);
+            });
     });
+
+
