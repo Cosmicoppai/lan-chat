e1aa4864b0eed2f961dc6b853b1a1caaa855474d
CosmicOppai <kanakchaudhari12@gmail.com>
2021-12-27 23:48:10 +0530

    added the msg writer for payloadLen > 125

--- chat/chatHandler.go
+++ chat/chatHandler.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 )
@@ -26,13 +27,16 @@ func Serve(conn net.Listener) error {
 func startChat(conn net.Conn) {
 	defer conn.Close()
 
-	// buff := make([]byte, 1024)      // make buffer of 1024 bytes
+	// buff := make([]byte, 4096)      // make buffer of 1024 bytes
 	if connectionUpgrade(conn) { // if connection upgrade is successful
-		read := bufio.NewReaderSize(conn, 40966)
+		read := bufio.NewReaderSize(conn, 5012)
 
 		for {
 			byte1, err := read.ReadByte()
 			if err != nil {
+				if err == io.EOF { // if user has closed the connection
+					break
+				}
 				fmt.Println("Error!: ", err)
 				return
 			}
@@ -43,24 +47,23 @@ func startChat(conn net.Conn) {
 			isMaskBitSet := byte2&maskBit != 0 // check if first bit of 2nd byte is set or not
 
 			var payloadLen int
+			var nbs []byte // series of bytes to store if payLoadLen > 125
 			if int(byte2&0x7f) <= 125 {
 				payloadLen = int(byte2 & 0x7f)
 			} else if byte2&0x7f == 126 { // Read the next 16 bits
 				nb1, _ := read.ReadByte()
 				nb2, _ := read.ReadByte()
-				mbs := []byte{nb1, nb2}
-				payloadLen = int(binary.BigEndian.Uint16(mbs))
+				nbs = append(nbs, []byte{nb1, nb2}...)
+				payloadLen = int(binary.BigEndian.Uint16(nbs))
 			} else {
-				var nb []byte
 				for i := 0; i < 8; i++ { // Read the next 64 bits
 					_nb, _ := read.ReadByte()
-					nb = append(nb, _nb)
+					nbs = append(nbs, _nb)
 				}
-				payloadLen = int(binary.BigEndian.Uint64(nb))
+				payloadLen = int(binary.BigEndian.Uint64(nbs))
 			}
 
 			if isMaskBitSet && (opCode == TextMessage) && isFinalBit != 0 {
-				fmt.Println("payLoad", payloadLen)
 				const maskKeyLen int = 4 // length of maskKeyLen
 				remBytes, _ := read.Peek(maskKeyLen + payloadLen)
 				maskKey := remBytes[:maskKeyLen]
@@ -71,17 +74,29 @@ func startChat(conn net.Conn) {
 					decodedPayload[i] = encodedPayLoad[i] ^ maskKey[i%maskKeyLen]
 
 				}
-				fmt.Println(string(decodedPayload))
 				_, _ = read.Discard(read.Buffered()) // Reset the buffer
-				msg := []byte{isFinalBit | opCode, byte(payloadLen)}
-				finMsg := append(msg, decodedPayload...)
-				_, _ = conn.Write(finMsg)
-				fmt.Println(read)
+
+				var payloadLenBytes []byte
+				if payloadLen == int(byte2&0x7f) {
+					payloadLenBytes = []byte{byte(payloadLen)}
+				} else {
+					payloadLenBytes = []byte{byte2 & 0x7f}
+					payloadLenBytes = append(payloadLenBytes, nbs...)
+				}
+
+				msg := []byte{isFinalBit | opCode}         // add the first byte consist of isFinalBit + OpCode
+				finMsg := append(msg, payloadLenBytes...)  // add the bytes consist of payloadLen
+				finMsg = append(finMsg, decodedPayload...) // add the message
+				_, err = conn.Write(finMsg)
+				if err != nil {
+					fmt.Println(err)
+				}
 
 			} else {
 				_, _ = read.Discard(read.Buffered())
 			}
 		}
+		conn.Close()
 	}
 	// t := http.ResponseWriter(conn)
 	_, _ = conn.Write([]byte(string(rune(http.StatusBadRequest))))
