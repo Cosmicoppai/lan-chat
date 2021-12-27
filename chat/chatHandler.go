6806fda135801537c45151e3a5d8d2c15fb488e0
CosmicOppai <kanakchaudhari12@gmail.com>
2021-12-27 19:20:59 +0530

    added the msg handling of len > 125

--- chat/chatHandler.go
+++ chat/chatHandler.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"net/http"
@@ -27,22 +28,39 @@ func startChat(conn net.Conn) {
 
 	// buff := make([]byte, 1024)      // make buffer of 1024 bytes
 	if connectionUpgrade(conn) { // if connection upgrade is successful
-		read := bufio.NewReader(conn)
+		read := bufio.NewReaderSize(conn, 40966)
 
 		for {
-			peek, err := read.ReadByte()
+			byte1, err := read.ReadByte()
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("Error!: ", err)
 				return
 			}
-			// fmt.Println("byte1", peek)
-			isFinalBit := peek & finalBit
-			opCode := peek & 0xf
-			peek2, _ := read.ReadByte()        // Read the next byte
-			isMaskBitSet := peek2&maskBit != 0 // check if first bit of 2nd byte is set or not
-			payloadLen := int(peek2 & 0x7f)
-			// fmt.Println("finalBit", isFinalBit, "isMaskBitSet",isMaskBitSet, "opCode", opCode, "payLen", payloadLen)
+
+			isFinalBit := byte1 & finalBit
+			opCode := byte1 & 0xf
+			byte2, _ := read.ReadByte()        // Read the next byte
+			isMaskBitSet := byte2&maskBit != 0 // check if first bit of 2nd byte is set or not
+
+			var payloadLen int
+			if int(byte2&0x7f) <= 125 {
+				payloadLen = int(byte2 & 0x7f)
+			} else if byte2&0x7f == 126 { // Read the next 16 bits
+				nb1, _ := read.ReadByte()
+				nb2, _ := read.ReadByte()
+				mbs := []byte{nb1, nb2}
+				payloadLen = int(binary.BigEndian.Uint16(mbs))
+			} else {
+				var nb []byte
+				for i := 0; i < 8; i++ { // Read the next 64 bits
+					_nb, _ := read.ReadByte()
+					nb = append(nb, _nb)
+				}
+				payloadLen = int(binary.BigEndian.Uint64(nb))
+			}
+
 			if isMaskBitSet && (opCode == TextMessage) && isFinalBit != 0 {
+				fmt.Println("payLoad", payloadLen)
 				const maskKeyLen int = 4 // length of maskKeyLen
 				remBytes, _ := read.Peek(maskKeyLen + payloadLen)
 				maskKey := remBytes[:maskKeyLen]
@@ -53,11 +71,12 @@ func startChat(conn net.Conn) {
 					decodedPayload[i] = encodedPayLoad[i] ^ maskKey[i%maskKeyLen]
 
 				}
-				// fmt.Println(string(decodedPayload))
+				fmt.Println(string(decodedPayload))
 				_, _ = read.Discard(read.Buffered()) // Reset the buffer
 				msg := []byte{isFinalBit | opCode, byte(payloadLen)}
 				finMsg := append(msg, decodedPayload...)
 				_, _ = conn.Write(finMsg)
+				fmt.Println(read)
 
 			} else {
 				_, _ = read.Discard(read.Buffered())
