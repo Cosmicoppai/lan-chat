af3e82273bcb9b3b38126978de44cb071525da94
tnaahsus <72088338+tnaahsus@users.noreply.github.com>
2022-01-02 15:34:55 +0530

    Update message.js

--- templates/js/message.js
+++ templates/js/message.js
@@ -1,38 +1,3 @@
-document.getElementById('sendButton').addEventListener('click', () => {
-    let textarea = document.getElementById('sendMessage').value;
-    let select = document.getElementById("inputState").value
-    let result
-    if ( select == 'markdown'){
-        result = marked.parse(textarea);
-    }
-    else{
-        result = textarea
-    }
-    console.log(result)
-    function clear() {
-        document.getElementById("sendMessage").value = "";
-        document.querySelector('textarea').style.cssText ='height:65px'
-    }
-    fetch("/",
-        {
-            method: "post",
-            body: JSON.stringify({
-                message:result
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response => {
-            if (response.ok) {
-                clear()
-            } else {
-                alert("Couldn't send your message!");
-                clear()
-            }
-        })
-
-})
-
 const fileFunction = ()=>{
         let data = document.getElementById("file").files[0];
         document.getElementById('fileName').innerHTML = 'Do you want to send '+ data.name
