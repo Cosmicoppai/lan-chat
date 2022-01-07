e2d2d9fa02e1375cdabb01abd2b72527d22dc8f5
Sushant <sushantshetty2001@gmail.com>
2022-01-08 01:24:38 +0530

    Error Handling

--- templates/js/imgModal.js
+++ templates/js/imgModal.js
@@ -1,32 +1,14 @@
 let modal = document.getElementById("myModal");
-let img = document.getElementById("imgSender");
-let img2 = document.getElementById("imgSender2");
 let modalImg = document.getElementById("img01");
 let captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-// img2.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-img.addEventListener('click', function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-});
+function imgClick() {
+    let img = document.getElementById("imgSender");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
 let span = document.getElementsByClassName("close")[0];
-span.addEventListener('click', function () {
-  modal.style.display = "none";
-});
-img2.addEventListener('click', function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-});
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
