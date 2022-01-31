79a53d23a9d9053b2a4e36c51523f773793dd90f
Sushant <sushantshetty2001@gmail.com>
2022-01-31 19:57:40 +0530

    Removed reload during light/dark toggle

--- templates/js/suggestions.js
+++ templates/js/suggestions.js
@@ -1,4 +1,6 @@
 document.getElementById('button').addEventListener('click', () => {
+    document.getElementById('movieError').style.display = 'none'
+    document.getElementById('dateError').style.display = 'none'
     document.getElementById('button').disabled = true;
     let moviefield = document.getElementById('moviefield').value;
     let datefield = document.getElementById('datefield').value;
@@ -9,24 +11,41 @@ document.getElementById('button').addEventListener('click', () => {
         document.getElementById("datefield").value = "";
         document.getElementById('button').disabled = false;
     }
-    let formData = new URLSearchParams();
-    formData.append('movie_name', moviefield);
-    formData.append('date', datefield);
-    formData.append('msg', descriptionfield);
-    fetch("/send-suggestion",
-    {   
-        body: formData,
-        method: "post",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(response => {
-        if (response.ok) { 
-            alert("Suggestion sent successfully!")
-            clear()
-          } else {
-            alert("Error!!!!");
-          }
-    })
-    
+    if (moviefield == '' && datefield == '') {
+        document.getElementById('movieError').style.display = 'block'
+        document.getElementById('dateError').style.display = 'clock'
+        document.getElementById('button').disabled = false;
+    }
+    else if (moviefield == '') {
+        document.getElementById('movieError').style.display = 'block'
+        document.getElementById('button').disabled = false;
+    }
+    else if (datefield == '') {
+        document.getElementById('dateError').style.display = 'block'
+        document.getElementById('button').disabled = false;
+    }
+    else if (moviefield && datefield) {
+        let formData = new URLSearchParams();
+        formData.append('movie_name', moviefield);
+        formData.append('date', datefield);
+        formData.append('msg', descriptionfield);
+        fetch("/send-suggestion",
+            {
+                body: formData,
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(response => {
+                    if (response.ok) {
+                        alert("Suggestion sent successfully!")
+                        clear()
+                    }
+                    else {
+                        alert("Error!!!!");
+                    }
+               
+            })
+    }
+
 })
