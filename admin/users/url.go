9d6da31e429629ab03b19d3ecda4e22ceeedff6b
cosmicoppai <kanakchaudhari12@gmail.com>
2022-04-26 12:44:28 +0530

    fix middleware issue in user API

--- admin/users/url.go
+++ admin/users/url.go
@@ -10,7 +10,7 @@ var Routes = []utils.Route{
 	utils.NewRoute(http.MethodPost, "/login", http.HandlerFunc(login)),
 	utils.NewRoute(http.MethodGet, "/users", middleware.AdminMiddleware(listUsers)),
 	utils.NewRoute(http.MethodPost, "/user/", middleware.AdminMiddleware(registerUser)),
-	utils.NewRoute(http.MethodGet, "/user/([^/]+)", middleware.AdminMiddleware(listUser)),
-	utils.NewRoute(http.MethodPut, "/user/", middleware.AdminMiddleware(updateUsername)),
-	utils.NewRoute(http.MethodDelete, "/user/([^/]+)", middleware.AdminMiddleware(deleteUser)),
+	utils.NewRoute(http.MethodGet, "/user/([^/]+)", middleware.AuthMiddleware(listUser)),
+	utils.NewRoute(http.MethodPut, "/user/", middleware.AuthMiddleware(updateUsername)),
+	utils.NewRoute(http.MethodDelete, "/user/([^/]+)", middleware.AuthMiddleware(deleteUser)),
 }
